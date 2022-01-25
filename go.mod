52387b5a8ece5d335ea68885d49952510bdb4dbb
l33th <l33th@protonmail.ch>
2022-01-25 07:20:00 +0200

    Handled Request Function

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/l33th/go-lambda-aws
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.28.0 // indirect
+require github.com/aws/aws-lambda-go v1.28.0
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
+func main() {
+	lambda.Start(HandleRequest)
+}
+
+func HandleRequest(req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	return events.APIGatewayV2HTTPResponse {
+		StatusCode: 200,
+		Body: "GoLang Lambda AWS API",
+	}, nil
 }
